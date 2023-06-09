7652776a3da23fa383a7f61b64757c001d632044
s1300051 <s1300051@u-aizu.ac.jp>
2023-06-09 17:53:12 +0900

    [Add] index

--- DiceGame.java
+++ DiceGame.java
@@ -4,20 +4,19 @@ public class DiceGame
 {
     public static void main(String[] args)
     {
-	DiceGame d = new DiceGame();
-	int die1 = d.getDice(), die2 = d.getDice();
+	int die1 = getDice(), die2 = getDice();
 
 	System.out.println("Rolling dice...");
 	System.out.println("Die 1: " + die1);
 	System.out.println("Die 2: " + die2);
-	System.out.println("Total value: " + d.getValue(die1, die2));
+	System.out.println("Total value: " + getValue(die1, die2));
     }
 
-    public int getDice()
+    public static int getDice()
     {
 	Random rand = new Random();
 	return rand.nextInt(6) + 1;
     }
 
-    public int getValue(int x, int y) {return x + y;}
+    public static int getValue(int x, int y) {return x + y;}
 }
