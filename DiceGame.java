c5f3e9bffc0177449d2a99e6155fab3e4b53db41
s1300051 <s1300051@u-aizu.ac.jp>
2023-06-10 21:21:26 +0900

    Dice game: initial revision

--- DiceGame.java
+++ DiceGame.java
@@ -4,18 +4,18 @@ public class DiceGame
 {
     public static void main(String[] args)
     {
-	int die1 = getDice(), die2 = getDice();
+        int die1 = getDice(), die2 = getDice();
 
-	System.out.println("Rolling dice...");
-	System.out.println("Die 1: " + die1);
-	System.out.println("Die 2: " + die2);
-	System.out.println("Total value: " + getValue(die1, die2));
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1: " + die1);
+        System.out.println("Die 2: " + die2);
+        System.out.println("Total value: " + getValue(die1, die2));
     }
 
     public static int getDice()
     {
-	Random rand = new Random();
-	return rand.nextInt(6) + 1;
+        Random rand = new Random();
+        return rand.nextInt(6) + 1;
     }
 
     public static int getValue(int x, int y) {return x + y;}
